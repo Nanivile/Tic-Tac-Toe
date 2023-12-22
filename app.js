be596c10369e8b39adf87a2902c9902bb3558ff0
Yeani <ifeanyi200019@gmail.com>
2023-12-22 11:17:01 -0500

    added a score board

--- app.js
+++ app.js
@@ -1,47 +1,66 @@
-
-
-
 const LOOKUP = {
     "1": "X",
     "-1": "O"
 };
 
-const combos = [ 
+const combos = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
     [2, 4, 6]
 ];
+
 let board;
 let turn;
 let winner;
+let score;
 
 const boardEl = document.getElementById("board");
-const restartBtn =document.getElementById("restartBtn");
+const restartBtn = document.getElementById("restartBtn");
+const player1ScoreEl = document.getElementById("player1Score");
+const player2ScoreEl = document.getElementById("player2Score");
+const tieScoreEl = document.getElementById("tieScore");
 
 boardEl.addEventListener("click", handleClick);
 restartBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    restart();
+    for (let i = 0; i < board.length; i++) {
+        const sq = document.getElementById(i);
+        sq.innerHTML = "";
+    }
+
+    init();
+    renderScore();
 });
 
 function init() {
     board = [null, null, null, null, null, null, null, null, null];
     turn = 1;
     winner = null;
+    score = {
+        player1: 0,
+        player2: 0,
+        tie: 0
+    };
 }
 
+init();
+
 function handleClick(evt) {
-    if (board[parseInt(evt.target.id)] ||
-    winner) return;
-    
+    if (board[parseInt(evt.target.id)] || winner) return;
+
     board[parseInt(evt.target.id)] = turn;
     turn *= -1;
     winner = checkWinner();
+
+    if (winner) {
+        updateScore(winner);
+        renderScore();
+    }
+
     render();
 }
 
@@ -65,15 +84,18 @@ function checkWinner() {
     return null;
 }
 
-function restart() {
-    init(); // Reset the game state
-
-    // Clear the board display
-    for (let i = 0; i < board.length; i++) {
-        const sq = document.getElementById(i);
-        sq.innerHTML = "";
+function updateScore(winner) {
+    if (winner === "X") {
+        score.player1++;
+    } else if (winner === "O") {
+        score.player2++;
+    } else {
+        score.tie++;
     }
 }
 
-init();
-
+function renderScore() {
+    player1ScoreEl.textContent = score.player1;
+    player2ScoreEl.textContent = score.player2;
+    tieScoreEl.textContent = score.tie;
+}
